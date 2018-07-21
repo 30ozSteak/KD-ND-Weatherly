26823473e3e1fc9aace7816166cd3fc21446a733
Kiel Dehnert <kielzor@gmail.com>
2018-07-21 15:35:11 -0600

    Formnat currentWeather method in dataScrape

--- src/DataScrape.js
+++ src/DataScrape.js
@@ -1,6 +1,13 @@
 export const currentWeather = data => {
   const currentDay = data.forecast.simpleforecast.forecastday.find(currentDay => currentDay.period === 1)
-  const currentDayObj = { time: data.current_observation.observation_time, location: data.current_observation.display_location.full, current:data.current_observation.temp_f + '•F', high: currentDay.high.fahrenheit + '•F', low: currentDay.low.fahrenheit + '•F', conditions: currentDay.conditions, icon: currentDay.icon_url }
+  const currentDayObj = { 
+    time: data.current_observation.observation_time, 
+    location: data.current_observation.display_location.full, 
+    current: data.current_observation.temp_f + '•F', 
+    high: currentDay.high.fahrenheit + '•F', 
+    low: currentDay.low.fahrenheit + '•F', conditions: currentDay.conditions, 
+    icon: currentDay.icon_url 
+  }
   return  currentDayObj;
 }
 
