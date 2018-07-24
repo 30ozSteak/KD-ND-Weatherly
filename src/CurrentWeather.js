5c7adb05e7d34b30809ee5f8195ef1e7984ca273
Nick Dambrosio <ndambro303@gmail.com>
2018-07-24 00:54:34 -0600

    WIP functions for hiding / showing seven hour / 10 day / welcome screen

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CurrentWeather = (props) => {
   return (
     <div className='current-weather'>
-      <h1>Currently in {props.weather.location}</h1>
+      <h1 className='currently-in-text'>Currently in {props.weather.location}</h1>
       <p className='current-temp'>{ props.weather.current }</p>
       <div className="current-bottom-row">
         <p>High: { props.weather.high }</p>
