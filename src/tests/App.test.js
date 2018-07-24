3905dd0b4b99f6458c70230cd1318e3fb2ce35cc
Kiel Dehnert <kielzor@gmail.com>
2018-07-23 21:09:35 -0600

    Add test files for all components

---
+++ src/tests/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
+import App from '../App';
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  })
+
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  })
+})
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+// export default App.test; 
