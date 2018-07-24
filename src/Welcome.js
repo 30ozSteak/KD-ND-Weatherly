a4a5fdd5825c964de584d83901ad1b840772c9aa
Nick Dambrosio <ndambro303@gmail.com>
2018-07-23 21:05:02 -0600

    Add gradients

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,7 +4,9 @@ import React from 'react';
 const Welcome = (props) => {
   return (
     <div className = "welcomeToWeatherly">
-      <h1 className = "welcomeText">Weatherly Baybeeee</h1>
+      <h1 className = "welcomeText">Hello Nick</h1>
+      {/* add a name entering component */}
+      <h5 className = "welcomeTextTwo">Welcome to Weatherly</h5>
     </div>  
   )
 
