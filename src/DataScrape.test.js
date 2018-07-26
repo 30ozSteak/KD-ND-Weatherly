7f2d60e80e142f9bf7b88bad0c80afef305597bb
Kiel Dehnert <kielzor@gmail.com>
2018-07-26 15:04:22 -0600

    Add tests for DataScrape

---
+++ src/DataScrape.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { currWeatherData } from './DataScrape';
+import { data } from './MockData';
+
+describe('DataScrape', () => {
+  it('should exist', () => {
+    expect(currWeatherData).toBeDefined();
+  })
+
+  it('should return the data filtered out but the method', () => {
+    let wrapper = mount(<currWeatherData />)
+    let cleanData = currWeatherData(data);
+
+    expect(cleanData).toEqual({
+                                "conditions": "Partly Cloudy", 
+                                "current": "46°F", 
+                                "high": "51°F", 
+                                "icon": "partlycloudy", 
+                                "location": "Louisville, KY", 
+                                "low": "32°F", 
+                                "time": "Last Updated on December 20, 11:27 AM EST"
+    })
+  })
+})
