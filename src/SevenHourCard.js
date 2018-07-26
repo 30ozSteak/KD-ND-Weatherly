677b91b48124912ade1d77aeebb821e65962eeee
Nick Dambrosio <ndambro303@gmail.com>
2018-07-26 13:56:26 -0600

    Add Seven Hour Card Tests

--- src/SevenHourCard.js
+++ src/SevenHourCard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 const SevenHourCard = (props) => {
   return (
     <div className = "seven-hour-section">
-      <h1>{props.time}</h1>
+      <h1 className ="seven-hour-time">{props.time}</h1>
       <h1 className="seven-hour-temp">{props.temp}°F</h1>
       <img className="seven-hour-icon" src={require(`./img/${props.icon}.png`)}/>
     </div>
---
+++ src/SevenHourCard.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import SevenHourCard from './SevenHourCard.js'
+
+describe('SevenHourCard', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow( <SevenHourCard time = "5pm" temp ="9001" icon = "Cloudy" /> )
+  });
+
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  })
+
+  it('should render to the page properly', () => {
+    const sevenHourCard = wrapper.find('seven-hour-section');
+    const sevenHourCardTime = wrapper.find('.seven-hour-time');
+    const sevenHourCardTemp = wrapper.find('.seven-hour-temp');
+    const sevenHourCardImg = wrapper.find('.seven-hour-icon');
+
+    expect (sevenHourCard).toBeDefined();
+    expect (sevenHourCardTime).toBeDefined();
+    expect (sevenHourCardTemp).toBeDefined();
+    expect (sevenHourCardImg).toBeDefined();
+    expect (sevenHourCardTime.text()).toEqual("5pm");
+    expect (sevenHourCardTemp.text()).toEqual("9001" + '°F');
+  })
+})
