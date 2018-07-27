37c4054d518ac30bbb004c19817ba0274eb5eb08
Nick Dambrosio <ndambro303@gmail.com>
2018-07-26 22:55:39 -0600

    Change position of gh logo

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,10 +5,9 @@ const Welcome = (props) => {
     <div className = "welcome-message" >
       <h1>Weatherly</h1>
       <h5 className>A Simple Weather Searching App</h5>
-      <p><span className = 'k'>Kiel</span> <a href="https://github.com/30ozSteak/KD-ND-Weatherly"><i className="fab fa-github"></i></a> Nick</p>
+      <a href="https://github.com/30ozSteak/KD-ND-Weatherly"><i className="fab fa-github"></i></a>
     </div>  
   )
-
 }
 
 export default Welcome;
